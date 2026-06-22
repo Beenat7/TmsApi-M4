b6478acf9a345e36629430f31848dc7a60bc473e
Beenatlay <beenatlaynigussie910@gmail.com>
2026-06-22 14:50:04 +0300

    feat: secure assessment results endpoint

--- Program.cs
+++ Program.cs
@@ -1,17 +1,50 @@
+using Microsoft.AspNetCore.Authentication;
+using TmsApi.Authentication;
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddAuthentication("DemoScheme")
+    .AddScheme<AuthenticationSchemeOptions, DemoAuthenticationHandler>(
+        "DemoScheme",
+        options => { });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+
+
+
+
+
+
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
